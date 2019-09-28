47083ca240e78d49bcfe6e97afa2907a21b5ba7d
Emilis Makutėnas <emilis.em4@gmail.com>
2019-09-28 13:20:52 +0300

    Add image controller

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsedVehicleParts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        // GET: api/Image
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        // GET: api/Image/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok("value");
+        }
+
+        // POST: api/Image
+        [HttpPost]
+        public ActionResult Post([FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // PUT: api/Image/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}

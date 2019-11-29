b08bae15fc3a04d006c52a83e7c66398aab2397e
Emilis Makutėnas <emilis.em4@gmail.com>
2019-11-29 03:10:48 +0200

    Add useFetch, add .env configurations

---
+++ UsedVehicleParts/ClientApp/src/Constants/api.tsx
@@ -0,0 +1,7 @@
+﻿const url = process.env.REACT_APP_API_HOST;
+
+const api = {
+  make: `${url}/make`
+};
+
+export default api;
---
+++ UsedVehicleParts/ClientApp/src/Utils/useFetch.tsx
@@ -0,0 +1,30 @@
+﻿import { useState, useEffect } from "react";
+
+const useFetch = (url: string, options: RequestInit) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url, options);
+      const json = await res.json();
+
+      setResponse(json);
+    } catch (e) {
+      setError(e);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+      fetchData();
+    },
+    []);
+
+  return [response, error, isLoading];
+}
+
+export default useFetch;

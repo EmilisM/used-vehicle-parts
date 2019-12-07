8c84c09a3e8c165d7c387a64245d2e0912f3d454
Emilis Makutėnas <emilis.em4@gmail.com>
2019-12-07 22:52:17 +0200

    Add IgnoreDataMember attribute to User Entity

--- UsedVehicleParts.API/DAL/Entities/UserData.cs
+++ UsedVehicleParts.API/DAL/Entities/UserData.cs
@@ -1,11 +1,16 @@
-﻿namespace UsedVehicleParts.API.DAL.Entities
+﻿using System.Runtime.Serialization;
+
+namespace UsedVehicleParts.API.DAL.Entities
 {
     public class UserData : Entity
     {
         public string Email { get; set; }
-        public string PasswordSalt { get; set; }
-        public string PasswordHash { get; set; }
         public int? Reputation { get; set; }
         public string ContactPhone { get; set; }
+
+        [IgnoreDataMember]
+        public string PasswordSalt { get; set; }
+        [IgnoreDataMember]
+        public string PasswordHash { get; set; }
     }
 }
--- UsedVehicleParts.API/Models/UserInfoModel.cs
+++ UsedVehicleParts.API/Models/UserInfoModel.cs
@@ -2,7 +2,6 @@
 {
     public class UserInfoModel
     {
-        public string Username { get; set; }
         public int? Reputation { get; set; }
         public string Email { get; set; }
         public string ContactPhone { get; set; }
